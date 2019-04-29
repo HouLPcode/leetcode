d731364565cfa91846d32d9b27f71c541d10dde7
hlp-pc <qq@qq.com>
2019-04-29 18:19:05 +0800

    1.two-sum.go 4ms 3.8MB

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -3,21 +3,23 @@
  *
  * [1] Two Sum
  */
-func twoSum(nums []int, target int) []int {
+ func twoSum(nums []int, target int) []int {
 	// map 存储值
 	cnt := []int{}
 	nummap := make(map[int]int,0)
 	// nums中的数值为k，位置为val
 	// 没必要首先遍历完所有数据，浪费时间和空间
 	for k,v := range nums{
-		// 注意，数值不能重复使用 [3,2,4] 6， 没有提前遍历一边，不存在重复使用数据问题
-		if val,ok := nummap[target - v]; ok{
+		nummap[v] = k
+	}
+
+	for k,v := range nums{
+		// 注意，数值不能重复使用 [3,2,4] 6
+		if val,ok := nummap[target - v]; ok && k != val{
 			cnt = append(cnt,k)
 			cnt = append(cnt,val)
 			break
-		} else {
-			nummap[v] = k
-		}
+		} 
 	}
 	return cnt
 }

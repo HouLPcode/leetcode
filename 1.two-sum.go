5b0876af2148a44d5028c2b960f32dbbd4fe8583
hlp-pc <qq@qq.com>
2019-04-29 18:10:29 +0800

    1.two-sum.go 减少代码行数,运行时间增加

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -10,16 +10,14 @@ func twoSum(nums []int, target int) []int {
 	// nums中的数值为k，位置为val
 	// 没必要首先遍历完所有数据，浪费时间和空间
 	for k,v := range nums{
-		nummap[v] = k
-	}
-
-	for k,v := range nums{
-		// 注意，数值不能重复使用 [3,2,4] 6
-		if val,ok := nummap[target - v]; ok && k != val{
+		// 注意，数值不能重复使用 [3,2,4] 6， 没有提前遍历一边，不存在重复使用数据问题
+		if val,ok := nummap[target - v]; ok{
 			cnt = append(cnt,k)
 			cnt = append(cnt,val)
 			break
-		} 
+		} else {
+			nummap[v] = k
+		}
 	}
 	return cnt
 }

11c5d33dfa4e0ac5028ca2368324aba388724940
hlp-pc <qq@qq.com>
2019-04-29 17:50:59 +0800

    1.two-sum.go ok

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -7,13 +7,14 @@ func twoSum(nums []int, target int) []int {
 	// map 存储值
 	cnt := []int{}
 	nummap := make(map[int]int,0)
+	// nums中的数值为k，位置为val
 	for k,v := range nums{
 		nummap[v] = k
 	}
 
 	for k,v := range nums{
 		// 注意，数值不能重复使用 [3,2,4] 6
-		if val,ok := nummap[target - v];ok{
+		if val,ok := nummap[target - v]; ok && k != val{
 			cnt = append(cnt,k)
 			cnt = append(cnt,val)
 			break

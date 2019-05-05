07c3c43271c5142b944284850ce1ec7804e91ec7
hlp-pc <qq@qq.com>
2019-05-05 20:42:07 +0800

    50.pow-x-n 非递归典型错误

--- 50.pow-x-n.go
+++ 50.pow-x-n.go
@@ -7,16 +7,16 @@ func myPow(x float64, n int) float64 {
   if n < 0{
 		return float64(1.0/myPow(x,-n))
 	}
-	//忘记了递归结束条件
-	if n==0{ // n==1 时多递归一次即可
-		return 1
+	// 非递归实现
+	out := float64(1.0)
+	for n != 0{
+		// 注意一下3个计算步骤的顺序
+		out *= out
+		if n&1 == 1{
+			out *= x // 奇数多乘一个x
+		}
+		n /= 2
 	}
-
-	if n&1 == 0{//偶数
-		// return float64(myPow(x,n/2) * myPow(x,n/2))// 这种方式有大量的重复运算，导致超时
-		return float64(myPow(x*x,n/2))
-	}
-	// return float64(myPow(x,n/2) * myPow(x,n/2) * x)
-	return float64(myPow(x*x,n/2) * x)
+	return out
 }
 

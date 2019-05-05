b082a3942dc7cdc28c048216d1405f05d3d43c0b
hlp-pc <qq@qq.com>
2019-05-05 16:55:16 +0800

    235.lowest-common-ancestor-of-a-binary-search-tree 非递归ok

--- 235.lowest-common-ancestor-of-a-binary-search-tree.go
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.go
@@ -11,17 +11,26 @@
  *     Right *ListNode
  * }
  */
- func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-  // 方法1，递归实现
-  // 根节点比要找的节点都大，则公共祖先肯定在左子树中
-  if root.Val > p.Val && root.Val > q.Val{
-	return lowestCommonAncestor(root.Left, p, q)
-  }
-  // 根节点比要找的节点都大，则公共祖先肯定在左子树中
-  if root.Val < p.Val && root.Val < q.Val{
-	return lowestCommonAncestor(root.Right, p, q)
-  }
-  return root 
-  // 方法2，非递归实现
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	// 方法1，递归实现
+	// 根节点比要找的节点都大，则公共祖先肯定在左子树中
+	//   if root.Val > p.Val && root.Val > q.Val{
+	// 		return lowestCommonAncestor(root.Left, p, q)
+	//   }
+	// 根节点比要找的节点都大，则公共祖先肯定在左子树中
+	//   if root.Val < p.Val && root.Val < q.Val{
+	// 	return lowestCommonAncestor(root.Right, p, q)
+	//   }
+	//   return root
+	// 方法2，非递归实现
+	for root != nil {
+		if root.Val > p.Val && root.Val > q.Val {
+			root = root.Left
+		} else if root.Val < p.Val && root.Val < q.Val {
+			root = root.Right
+		} else {
+			break
+		}
+	}
+	return root
 }
-

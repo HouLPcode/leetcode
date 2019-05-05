0e54922a4bc74d97a78cb65d4ddbc3492c0c6ef6
hlp-pc <qq@qq.com>
2019-05-05 20:45:50 +0800

    50.pow-x-n 非递归ok

--- 50.pow-x-n.go
+++ 50.pow-x-n.go
@@ -10,12 +10,11 @@ func myPow(x float64, n int) float64 {
 	// 非递归实现
 	out := float64(1.0)
 	for n != 0{
-		// 注意一下3个计算步骤的顺序
-		out *= out
 		if n&1 == 1{
 			out *= x // 奇数多乘一个x
 		}
-		n /= 2
+		x *= x
+		n >>= 1
 	}
 	return out
 }

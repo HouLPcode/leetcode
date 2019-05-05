74d0662f6e7a420f9bb8fbde87ffcab338748594
hlp-pc <qq@qq.com>
2019-05-05 17:27:19 +0800

    236.lowest-common-ancestor-of-a-binary-tree 递归ok

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=236 lang=golang
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+/**
+ * Definition for TreeNode.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *ListNode
+ *     Right *ListNode
+ * }
+ */
+ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+  if root == nil || root.Val == p.Val || root.Val == q.Val{
+	  return root
+  } 
+  left := lowestCommonAncestor(root.Left,p,q)
+  right := lowestCommonAncestor(root.Right,p,q)
+  // 此处为空的意义？？？
+  if left == nil {
+	  return right
+  }else if right == nil{
+	return left
+  }
+  return root
+}
+

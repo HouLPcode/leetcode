e6a653ec05ae8fac0bb69d255dc6ca1ca8d45b60
hlp-pc <qq@qq.com>
2019-05-06 11:26:52 +0800

    104.maximum-depth-of-binary-tree 递归ok

---
+++ 104.maximum-depth-of-binary-tree.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=104 lang=golang
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+ // 递归实现
+func maxDepth(root *TreeNode) int {
+    if root == nil{
+		return 0
+	}
+	ldep := maxDepth(root.Left)
+	rdep := maxDepth(root.Right)
+	if ldep < rdep{
+		return 1 + rdep
+	}
+	return 1 + ldep
+}
+

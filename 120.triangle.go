e63f14ea3c35193edfe0baf4a7f051aaf81f6238
侯利朋 <houlipengdyx@163.com>
2019-05-07 13:44:21 +0800

    120.triangle 递归 超时 map缓存 ok

--- 120.triangle.go
+++ 120.triangle.go
@@ -11,20 +11,27 @@ func minimumTotal(triangle [][]int) int {
 
 	//???怎么range二维slice
 	rows,columns := len(triangle),len(triangle[len(triangle)-1])
-	rnt := help(0,0,rows,columns,triangle)
+	mems := make(map[string]int)
+	rnt := help(0,0,rows,columns,triangle,mems)
 	return rnt
 }
 
-// 递归实现
-func help(i,j,rows,cols int, nums [][]int) int{
+// 递归实现，超时
+func help(i,j,rows,cols int, nums [][]int, mems map[string]int) int{
+	if v,ok := mems[fmt.Sprint(i,"+",j)]; ok{
+		return v
+	}
 	if i == rows - 1{
+		mems[fmt.Sprint(i,"+",j)] = nums[i][j]
 		return nums[i][j]
 	}
-	n1 := help(i+1,j,rows,cols,nums)
-	n2 := help(i+1,j+1,rows,cols,nums)
+	n1 := help(i+1,j,rows,cols,nums,mems)
+	n2 := help(i+1,j+1,rows,cols,nums,mems)
 	if n1 < n2{
+		mems[fmt.Sprint(i,"+",j)] = nums[i][j] + n1
 		return nums[i][j] + n1
 	}
+	mems[fmt.Sprint(i,"+",j)] = nums[i][j] + n2
 	return nums[i][j] + n2 
 }
 

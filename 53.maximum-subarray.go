9a398a8c0d6e6c2452cfb07037186c2819181fb6
侯利朋 <houlipengdyx@163.com>
2019-05-08 20:10:47 +0800

    53.maximum-subarray ok

--- 53.maximum-subarray.go
+++ 53.maximum-subarray.go
@@ -5,25 +5,23 @@
  */
 func maxSubArray(nums []int) int {
 	// f(i) 包含nums[i]的和的最大值
-	// 连续， f(i) = max( f(i-1)+nums[i],nums[i])
+	// 连续， f(i) = max( f(i-1)+nums[i],nums[i]) 
 	// 结果 max( f(0...i) )
+	// 转移方程只和前一个数有关，只缓存前一个计算结果即可
 	if nums == nil || len(nums) == 0{
 		return 0
 	}
-	buf := make([]int,len(nums),len(nums))
-	curMax := nums[0]
-	for k,v := range nums{
-		if k == 0{
-			buf[k] = v
-		}else if buf[k-1] > 0{
-			buf[k] = buf[k-1]+v
-		}else{
-			buf[k] = v
-		}
-		if curMax < buf[k]{
-			curMax = buf[k]
-		}
+	curMax, sum := nums[0], nums[0]
+	for _,v := range nums[1:]{
+		sum = max(sum+v,v)
+		curMax = max(curMax,sum)
 	}
 	return curMax
+}
+func max(a,b int)int{
+	if a > b{
+		return a
+	}
+	return b
 }
 

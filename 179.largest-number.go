0b3a7a708fa0ba6ceb359bf0210f0870d896c06b
侯利朋 <houlipengdyx@163.com>
2019-05-12 19:38:49 +0800

    179.largest-number err 超时

--- 179.largest-number.go
+++ 179.largest-number.go
@@ -7,12 +7,20 @@ func largestNumber(nums []int) string {
 	// 直接左右两边拼接成字符串后，取较大的那一个
 	// 典型错误，0存在的时候局部最优不是最终最优 [0,9,8,7,6,5,4,3,2,1]
 	str := ""
+	cnt0 := 0
 	for _,v := range nums{
+		if v == 0{
+			cnt0++
+			continue
+		}
 		if strings.Compare(str+fmt.Sprint(v),fmt.Sprint(v)+str) > 0{
 			str = str+fmt.Sprint(v)
 		} else{
 			str = fmt.Sprint(v) + str
 		}
+	}
+	for cnt0 > 0{
+		str += "0"
 	}
 	return str
 }

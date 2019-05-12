2bf994fcb821496ee6f394b6cbab2fff39fb2c89
侯利朋 <houlipengdyx@163.com>
2019-05-12 19:05:17 +0800

    171.excel-sheet-column-number.go ok string->rune

---
+++ 171.excel-sheet-column-number.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=171 lang=golang
+ *
+ * [171] Excel Sheet Column Number
+ */
+func titleToNumber(s string) int {
+	//可以理解成26进制编码
+	rnt := 0
+	for _,v := range []rune(s){
+		rnt = rnt * 26 + int(v - rune('A')) + 1
+	}
+	return rnt
+}
+

b528b291afe81f99f34aeab9bb805a6adfe59746
侯利朋 <houlipengdyx@163.com>
2019-05-14 23:35:29 +0800

    105.construct-binary-tree-from-preorder-and-inorder-traversal ok 递归

---
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=105 lang=golang
+ *
+ * [105] Construct Binary Tree from Preorder and Inorder Traversal
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+    if len(preorder) == 0 && len(inorder) == 0{
+		return nil
+	}
+	// 注意
+	// if len(preorder) == 1 && len(inorder) == 1{
+	// 	return &TreeNode{
+	// 		Val:preorder[0],
+	// 		Left:nil,
+	// 		Right:nil,
+	// 	}
+	// }
+
+	rootVal := preorder[0]	
+	// 没有重复元素
+	i := 0
+	for ; i < len(inorder); i++{
+		if inorder[i] == rootVal{
+			break
+		}
+	}
+
+	// 一个底层slice的多个值赋值会导致什么问题吗？？？	
+	var lpreorder,rpreorder,linorder,rinorder []int
+	if i == 0{//无左子树
+		rpreorder = preorder[1:]
+		rinorder = inorder[i+1:] //越界
+	}else if i == len(inorder) -1{//无右子树
+		lpreorder = preorder[1:]
+		linorder = inorder[:i]
+	}else{
+		linorder = inorder[:i]
+		rinorder = inorder[i+1:]
+		rpreorder = preorder[len(linorder)+1:]
+		lpreorder = preorder[1:len(linorder)+1]
+	}
+	
+	root := TreeNode{
+		Val:rootVal,
+		Left:buildTree(lpreorder,linorder),
+		Right:buildTree(rpreorder,rinorder)}
+	return &root
+}
+

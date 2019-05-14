fb083d48f471737ad5d20621c7bd8d4d40deb8ac
侯利朋 <houlipengdyx@163.com>
2019-05-14 11:48:55 +0800

    145.binary-tree-postorder-traversal ok  非递归,1个栈

--- 145.binary-tree-postorder-traversal.go
+++ 145.binary-tree-postorder-traversal.go
@@ -11,17 +11,46 @@
  *     Right *TreeNode
  * }
  */
+// func postorderTraversal(root *TreeNode) []int {
+// 	// 非递归，后续遍历
+// 	if root == nil{
+// 		return []int{}
+// 	}
+// 	rnt := []int{}
+// 	lefts := postorderTraversal(root.Left)
+// 	rights := postorderTraversal(root.Right)
+// 	rnt = append(rnt, lefts...)// 注意...不能少
+// 	rnt = append(rnt, rights...)
+// 	rnt = append(rnt, root.Val)
+// 	return rnt
+// }
+
+// 非递归，后序，1个栈
 func postorderTraversal(root *TreeNode) []int {
-	// 非递归，后续遍历
 	if root == nil{
 		return []int{}
 	}
+	var top *TreeNode = nil
+	stack := []*TreeNode{}
 	rnt := []int{}
-	lefts := postorderTraversal(root.Left)
-	rights := postorderTraversal(root.Right)
-	rnt = append(rnt, lefts...)// 注意...不能少
-	rnt = append(rnt, rights...)
-	rnt = append(rnt, root.Val)
+	// 头节点入栈
+	stack = append(stack, root)
+	for len(stack) > 0{
+		top = stack[len(stack)-1]
+
+		if top.Left!=nil && top.Left!=root && top.Right!=root{
+			stack = append(stack, top.Left)
+		}else if top.Right!=nil && top.Right!=root{
+			stack = append(stack, top.Right)
+		}else{
+			//访问栈顶元素
+			root = top
+			// 出栈
+			top,stack = stack[len(stack)-1],stack[:len(stack)-1]
+			// 访问站定元素
+			rnt = append(rnt, top.Val)
+		}
+	}
 	return rnt
 }
 

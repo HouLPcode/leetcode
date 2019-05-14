710b070ef21e251fa2a77496fae40b67ebde8a4a
侯利朋 <houlipengdyx@163.com>
2019-05-14 23:48:06 +0800

    617.merge-two-binary-trees ok 递归

---
+++ 617.merge-two-binary-trees.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=617 lang=golang
+ *
+ * [617] Merge Two Binary Trees
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	// 递归实现
+	if t1 == nil{
+		return t2
+	}
+	if t2 == nil{
+		return t1
+	}
+	return &TreeNode{
+		Val:t1.Val+t2.Val,
+		Left:mergeTrees(t1.Left,t2.Left),
+		Right:mergeTrees(t1.Right,t2.Right),
+	}
+}
+

36a460fe028fd3415112d60cde4518c0414f112a
侯利朋 <houlipengdyx@163.com>
2019-05-17 23:45:14 +0800

    28.implement-str-str ok待优化

---
+++ 28.implement-str-str.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=28 lang=golang
+ *
+ * [28] Implement strStr()
+ */
+ // "aaa"\n"aaaa" 注意needle在haystack末尾时的情况
+func strStr(haystack string, needle string) int {
+    if len(needle) == 0{
+		return 0
+	}
+	for i:=0; i<len(haystack); i++{
+		if haystack[i] == needle[0]{
+			j:=1
+			for ; j<len(needle)&&i+j<len(haystack)&&haystack[i+j] == needle[j];j++{}
+			if j == len(needle){
+				return i
+			}
+		}
+	}
+	return -1
+}
+

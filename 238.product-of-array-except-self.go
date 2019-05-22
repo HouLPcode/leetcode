0bde2cb4ae31c4ff46070ffc0d7c5605a5ce365c
侯利朋 <houlipengdyx@163.com>
2019-05-22 10:20:10 +0800

    238.product-of-array-except-self ok

--- 238.product-of-array-except-self.go
+++ 238.product-of-array-except-self.go
@@ -4,12 +4,13 @@
  * [238] Product of Array Except Self
  */
 func productExceptSelf(nums []int) []int {
-	arr := make([]int, len(nums))
+	// 题目中要求num长度大于1，且输出空间不算是额外开辟的空间
+	arr := make([]int, len(nums)) // 存储前i项乘积，不包含第i项
 	arr[0] = 1
 	for i:=1; i<len(nums); i++ {
 		arr[i] = arr[i-1] * nums[i-1]
 	}
-	tmp := 1
+	tmp := 1 // 从右往左的乘积
 	for i:=len(nums)-1; i>=0; i--{
 		arr[i] = tmp * arr[i]
 		tmp *= nums[i] 

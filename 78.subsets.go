dd57e1150030120f2729a4de9a29430d999d2d95
侯利朋 <houlipengdyx@163.com>
2019-05-23 11:13:12 +0800

    78.subsets err 输出顺序

--- 78.subsets.go
+++ 78.subsets.go
@@ -5,12 +5,14 @@
  */
  // 法1，回溯法
  // 法2，位运算，合理的值表示所有情况
+ // [9,0,3,5,7] 回溯法的顺序不满足答案要求？？？
 func subsets(nums []int) [][]int {
 	// 回溯法思路：
 	// generate i 对i元素进行放入和不放入的操作，后递归调动i+1 ，
 	// 结束条件：i大于最后一个元素
 	tmp := []int{}
-	result := [][]int{}
+	result := [][]int{[]int{}}//默认含有一个空子集
+	sort.Ints(nums)
 	generate(0, tmp, nums, &result)
 	return result
 }
@@ -19,12 +21,18 @@ func generate(i int, tmp, nums []int, result *[][]int){
 	if i == len(nums){
 		return 
 	}
+
 	// 不放入i元素
-	*result = append((*result), tmp)
+	// *result = append((*result), tmp)
 	generate(i+1, tmp, nums, result)
+	
 	// 放入i元素
 	tmp = append(tmp, nums[i])
-	*result = append((*result), tmp)	
+	// 注意，只有带i元素的结果需要，不带i元素的情况在上层递归前已经插入了
+	// 注意，少了一个元素也没有的子集
+	data := make([]int, len(tmp))
+	copy(data, tmp)
+	*result = append((*result), data)	
 	generate(i+1, tmp, nums, result)
 }
 

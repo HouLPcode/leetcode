cc4df65dc7e4bf1bc17b1027d0589d776c737a1c
侯利朋 <houlipengdyx@163.com>
2019-05-24 16:56:30 +0800

    290.word-pattern err 典型错误

--- 290.word-pattern.go
+++ 290.word-pattern.go
@@ -8,19 +8,24 @@
 func wordPattern(pattern string, str string) bool {
     // str 按空格分词后，跟pattern中的一个字符配对，str或pattern替换成另一个，然后匹配结果
 	strs := strings.Split(str, " ")
-	if len(strs) != len(pattern){//注意长度不匹配时直接报错
-		return false
-	}
-	alpMap := make(map[byte]string, 0)
+	alpMap := make(map[string]byte, 0)
 	for k,v := range strs{
-		// k 有越界的可能
-		// 错误："ab"\n"dog dog"
-		alpMap[pattern[k]] = v  // alpMap['a'] = "dog"
+		if k >= len(pattern){
+			return false
+		}
+		if _,ok := alpMap[v]; !ok { // v没出现过，但是pattern出现，导致结果错误
+			alpMap[v] = pattern[k] // alpMap["dog"] = 'a'
+		}else if alpMap[v] == pattern[k]{
+			continue
+		}else{
+			return false
+		}
 	}
-	tmpStr := ""
-	for i:=0; i<len(pattern); i++{
-		tmpStr += alpMap[pattern[i]] + " "//注意删除最后的空格
-	}
-	return tmpStr == str+" "
+	return true	
+	// tmpStr := ""
+	// for i:=0; i<len(strs); i++{
+	// 	tmpStr += alpMap[pattern[i]] + " "//注意删除最后的空格
+	// }
+	// return tmpStr == str+" "
 }
 

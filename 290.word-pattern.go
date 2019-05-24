b58179a00e01b47c0db61aecf5bd45f459194028
侯利朋 <houlipengdyx@163.com>
2019-05-24 14:46:04 +0800

    290.word-pattern err 典型错误

--- 290.word-pattern.go
+++ 290.word-pattern.go
@@ -3,11 +3,18 @@
  *
  * [290] Word Pattern
  */
+ // "aba"\n"cat cat cat dog"
+//  "abba"\n"dog dog dog dog"
 func wordPattern(pattern string, str string) bool {
     // str 按空格分词后，跟pattern中的一个字符配对，str或pattern替换成另一个，然后匹配结果
 	strs := strings.Split(str, " ")
+	if len(strs) != len(pattern){//注意长度不匹配时直接报错
+		return false
+	}
 	alpMap := make(map[byte]string, 0)
 	for k,v := range strs{
+		// k 有越界的可能
+		// 错误："ab"\n"dog dog"
 		alpMap[pattern[k]] = v  // alpMap['a'] = "dog"
 	}
 	tmpStr := ""

1eb051bd5898ab57f410bc9336df452b118483b7
侯利朋 <houlipengdyx@163.com>
2019-06-01 22:40:53 +0800

    167.two-sum-ii-input-array-is-sorted ok

--- 167.two-sum-ii-input-array-is-sorted.go
+++ 167.two-sum-ii-input-array-is-sorted.go
@@ -3,7 +3,7 @@
  *
  * [167] Two Sum II - Input array is sorted
  */
-func twoSum(numbers []int, target int) []int {
+ func twoSum(numbers []int, target int) []int {
 	i, j := 0, len(numbers)-1
 	//首尾逐步排查 O(n)
 	// 二分法可以吗？？？

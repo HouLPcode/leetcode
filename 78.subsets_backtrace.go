fd6a46f5dca6eb7fc425c73fa80da4e31d6bf33e
侯利朋 <houlipengdyx@163.com>
2019-06-03 10:04:34 +0800

    fix

--- 78.subsets_backtrace.go
+++ 78.subsets_backtrace.go
@@ -6,30 +6,22 @@
  // 法1，回溯法
  // 法2，位运算，合理的值表示所有情况
  // [9,0,3,5,7] 回溯法的顺序不满足答案要求？？？
-//  ✘ answer:          [[],[4],[1],[0],[1,4],[0,1],[0,1],[0,1,4]]
-//  ✘ expected_answer: [[],[4],[1],[1,4],[0],[0,4],[0,1],[0,1,4]]
- 
+
 func subsets(nums []int) [][]int {
 	result := [][]int{}
-	for i:=0; i<=len(nums); i++{
-		generate(0, i,[]int{}, nums, &result)
-	}
+	generate(0, []int{}, nums, &result)
 	return result
 }
 
-func generate(s int,n int, path []int, nums []int, result *[][]int){
-	if n == len(path){
-		data := make([]int, len(path))
-		copy(data, path)
-		sort.Ints(data)
-		*result = append((*result), data)
-		return 
-	}
+func generate(s int, path []int, nums []int, result *[][]int){
+	data := make([]int, len(path))
+	copy(data, path)
+	sort.Ints(data)
+	*result = append((*result), data)
 
 	for i:=s; i<len(nums); i++ {
 		path = append(path, nums[i])
-		generate(i+1, n, path, nums, result)
+		generate(i+1, path, nums, result)
 		path = path[:len(path)-1]
 	}
 }
-

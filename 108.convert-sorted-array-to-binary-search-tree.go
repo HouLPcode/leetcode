df1ee5e4970b5cd628d335a64ba2596eeed1577b
侯利朋 <houlipengdyx@163.com>
2019-06-06 10:55:45 +0800

    108.convert-sorted-array-to-binary-search-tree ok折半递归创建

--- 108.convert-sorted-array-to-binary-search-tree.go
+++ 108.convert-sorted-array-to-binary-search-tree.go
@@ -12,14 +12,17 @@
  * }
  */
 func sortedArrayToBST(nums []int) *TreeNode {
-	// 折半递归创建不行，不能保证创建的是平衡树
+	// 折半递归创建
 	if len(nums) == 0{
 		return nil
 	}
+	// 怎么保证创建的是平衡树
+	// AVL树的调整设计到左右旋转问题
+	// 因为常见节点的时候是尽可能的左右节点都有，创建的就是平衡树
 	return &TreeNode{
 		Val: nums[len(nums)/2],
 		Left: sortedArrayToBST(nums[:len(nums)/2]),
-		Right: sortedArrayToBST(nums[len(nums)/2+1:]),// +1不回越界
+		Right: sortedArrayToBST(nums[len(nums)/2+1:]),// +1不会越界
 	}
 }
 

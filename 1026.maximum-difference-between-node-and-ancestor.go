bca232b2dcbaf56efc3a7b23f7d51aedacc4e8de
侯利朋 <houlipengdyx@163.com>
2019-06-06 10:47:48 +0800

    1026.maximum-difference-between-node-and-ancestor ok DFS递归

--- 1026.maximum-difference-between-node-and-ancestor.go
+++ 1026.maximum-difference-between-node-and-ancestor.go
@@ -11,29 +11,42 @@
  *     Right *TreeNode
  * }
  */
-// 相当于找tree的最大最小节点，注意最大最小值不能在同一层
+// 相当于找tree的最大最小节点，
+// 一定注意，题目中要求两个最值节点不能在同一层。最大最小值不能在同一层
 func maxAncestorDiff(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-	max, min := root.Val, root.Val
-	queue := list.New()
-	queue.PushBack(root)
-	for queue.Front() != nil {
-		node := queue.Front()
-		queue.Remove(node)
-		if node.Value.(*TreeNode).Val > max {
-			max = node.Value.(*TreeNode).Val
-		}
-		if node.Value.(*TreeNode).Val < min {
-			min = node.Value.(*TreeNode).Val
-		}
-		if node.Value.(*TreeNode).Left != nil{
-			queue.PushBack(node.Value.(*TreeNode).Left)
-		}
-		if node.Value.(*TreeNode).Right != nil{
-			queue.PushBack(node.Value.(*TreeNode).Right)
-		}
-	}
-	return max - min
+	// 题目中给出节点值得范围 0-100000.
+	minnum := 100000
+	maxnum := 0
+	return getmaxAncestorDiff(root, minnum, maxnum)
 }
+
+// root为根节点，当前已知的最大最小值，返回最大差值
+// 怎么避免的最大最小值不在同一层？？？ DFS，访问的是路径上的最值，一条路径上不可能同时获取同一层的最值
+func getmaxAncestorDiff(root *TreeNode, minnum,maxnum int) int{
+	if root == nil {
+		return maxnum - minnum
+	}
+	// 访问当前节点
+	minnum = min(minnum, root.Val)
+	maxnum = max(maxnum, root.Val)
+	// 递归左右子树
+	leftMaxDiff := getmaxAncestorDiff(root.Left, minnum, maxnum) // 左子树最大差值
+	rightMaxDiff := getmaxAncestorDiff(root.Right, minnum, maxnum) // 右子树最大差值
+	// 返回最大数值差
+	return max(leftMaxDiff, rightMaxDiff)
+} 
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+

238f65c15116bf5c6a911e8422bb6ac6dae0fd2f
侯利朋 <houlipengdyx@163.com>
2019-06-11 16:16:42 +0800

    372.super-pow err 典型错误

--- 372.super-pow.go
+++ 372.super-pow.go
@@ -3,10 +3,11 @@
  *
  * [372] Super Pow
  */
+// 错误思路: 把b组成int，然后折半相乘 
+// ****b可以无限大，导致超时。b可以无限大，无法转换成int数值
+
 func superPow(a int, b []int) int {
 	//不考虑越界问题，指定的是int
-	// a==1 加速？？？
-	// 思路: 把b组成int，然后折半相乘
 	m := slice2int(b)
 	return pow(a,m)%1337
 }
@@ -18,9 +19,9 @@ func pow(x,m int)int{
 		return 1
 	}
 	if m&1 == 0{
-		return pow(x,m/2)*pow(x,m/2)
+		return (pow(x,m/2)%1337)*(pow(x,m/2)%1337)
 	}else{
-		return pow(x,m/2)*pow(x,m/2)*x
+		return (pow(x,m/2)%1337)*(pow(x,m/2)%1337)*x%1337
 	}
 }
 

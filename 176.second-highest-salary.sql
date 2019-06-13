27cb5b52d8d8dd1c9bf2bef2cd05e217339d118d
侯利朋 <houlipengdyx@163.com>
2019-06-13 17:49:39 +0800

    176.second-highest-salary ok 子查询-> null

--- 176.second-highest-salary.sql
+++ 176.second-highest-salary.sql
@@ -7,7 +7,9 @@
 # 通过LIMIT OFFSET 取出来第二个数
 # 注意排序后的Salary可能有重复元素，必须去重
 # 没有第二数时候需要返回null
-SELECT DISTINCT Salary AS SecondHighestSalary 
-FROM Employee
-ORDER BY Salary DESC 
-LIMIT 1 OFFSET 1; 
+SELECT (
+    SELECT DISTINCT Salary 
+    FROM Employee
+    ORDER BY Salary DESC 
+    LIMIT 1 OFFSET 1
+    ) AS SecondHighestSalary; 

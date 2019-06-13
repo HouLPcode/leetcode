ef6ea19c4841b27c56a68cb628455c24caf98abe
侯利朋 <houlipengdyx@163.com>
2019-06-13 17:46:10 +0800

    176.second-highest-salary err

--- 176.second-highest-salary.sql
+++ 176.second-highest-salary.sql
@@ -6,7 +6,8 @@
 # Write your MySQL query statement below
 # 通过LIMIT OFFSET 取出来第二个数
 # 注意排序后的Salary可能有重复元素，必须去重
-SELECT Salary AS SecondHighestSalary 
+# 没有第二数时候需要返回null
+SELECT DISTINCT Salary AS SecondHighestSalary 
 FROM Employee
 ORDER BY Salary DESC 
-LIMIT 1 OFFSET 1;
+LIMIT 1 OFFSET 1; 

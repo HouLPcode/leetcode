91cb58f5d24692738f8f100914fed2530bfe4c49
侯利朋 <houlipengdyx@163.com>
2019-06-14 13:34:27 +0800

    443.string-compression ok

--- 443.string-compression.go
+++ 443.string-compression.go
@@ -7,24 +7,24 @@
 // 注意需要保持字母的顺序
 func compress(chars []byte) int {
 	// key-value形式，如果v=1则只保留k
-	charMap := make([]int, 126-35+1)
-	for i:=0; i<len(chars); i++ {
-		charMap[chars[i]-35]++
-	}
 	index := 0
-	for k,v := range charMap {
-		if v == 1 {
-			chars[index] = byte(k+35)
-			index++
-		}else if v > 1{
-			chars[index] = byte(k+35)
-			index++
-			//注意v大于个位数的处理
-			times := fmt.Sprint(v)
-			for i:=0; i<len(times); i++ {
-				chars[index] = times[i]
+	for cur:=0; cur<len(chars);  {
+		// 先存字母
+		chars[index] = chars[cur]
+		index++
+		// 再存次数
+		times := 1
+		for next:=cur+1; next<len(chars)&&chars[cur]==chars[next];next++{
+			times++
+		}
+		// 更新当前索引cur
+		cur += times
+		if times > 1 {
+			tmp := fmt.Sprint(times)
+			for i:=0; i<len(tmp); i++ {
+				chars[index] = tmp[i]
 				index++
-			}
+			} 
 		}
 	}
 	return index //此处的index比实际大1，返回的是长度，所以不用-1处理

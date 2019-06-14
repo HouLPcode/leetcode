e541a6990fc067bd137be5c88b8a301144ef0fe8
侯利朋 <houlipengdyx@163.com>
2019-06-14 14:43:04 +0800

    482.license-key-formatting ok slice优化

--- 482.license-key-formatting.go
+++ 482.license-key-formatting.go
@@ -3,29 +3,33 @@
  *
  * [482] License Key Formatting
  */
-//  224ms  19.38%
+// 0ms
+// memory: 4.4MB 96.67%
 //  "2-4A0r7-4k"\n4
 // "--a-a-a-a--"\n2
+// "aaaa"\n2
 // 题目理解有误，除第一组外，其他每组均有K个元素
 // string的拼接通过[]byte->string加速
 func licenseKeyFormatting(S string, K int) string {
 	//从右往左处理
-	out := ""
+	out := make([]byte, len(S)*2) // out长度不一定小于S
 	times := 0
+	index := len(S)*2-1
 	for i:=len(S)-1; i>=0; i--{
 		if S[i] != '-' {
 			if times == K{ // 放在S[i]赋值前，避免最终结果头多一个 "-"
 				times = 0
-				out = "-" + out
+				out[index] = '-'
+				index--
 			}
 			times++
 			if S[i] >= 'a'{ // convert uppercase
-				out = string(S[i]-'a'+'A') + out
-			}else {
-				out = string(S[i]) + out
+				out[index] = S[i]-'a'+'A'
+			} else {
+				out[index] = S[i]
 			}
+			index--
 		}
 	}
-	return out 	// 去除头部 -
+	return string(out[index+1:]) 	// 去除头部 -
 }
-

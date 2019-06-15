573a69f72fd2579a1ab805864c9881bf8db993f3
侯利朋 <houlipengdyx@163.com>
2019-06-15 20:25:44 +0800

    183.customers-who-never-order ok 子查询或左联结

---
+++ 183.customers-who-never-order.sql
@@ -0,0 +1,20 @@
+--
+-- @lc app=leetcode id=183 lang=mysql
+--
+-- [183] Customers Who Never Order
+--
+-- 334 ms 5 %
+# Write your MySQL query statement below
+-- 左联结
+SELECT Customers.Name AS Customers
+FROM Customers LEFT JOIN Orders
+ON Customers.Id = Orders.CustomerId
+WHERE Orders.CustomerId IS NULL; 
+
+-- 子查询
+-- select customers.name as 'Customers'
+-- from customers
+-- where customers.id not in
+-- (
+--     select customerid from orders
+-- );

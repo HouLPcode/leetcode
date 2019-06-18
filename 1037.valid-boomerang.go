9b5b29881f4d4595dc5c283c258c49fa6c68618c
侯利朋 <houlipengdyx@163.com>
2019-06-18 09:26:14 +0800

    1037.valid-boomerang ok

---
+++ 1037.valid-boomerang.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1037 lang=golang
+ *
+ * [1037] Valid Boomerang
+ */
+//  0 ms
+func isBoomerang(points [][]int) bool {
+	//三点不共线返回true
+	// (y2-y1)/(x2-x1) == (y3-y2)/(x3-x2)
+	return (points[1][1]-points[0][1])*(points[2][0]-points[1][0]) != (points[2][1]-points[1][1])*(points[1][0]-points[0][0])
+}
+

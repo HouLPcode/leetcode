dd6e1861041da598b4257ed5f0ba2963da6f431b
侯利朋 <houlipengdyx@163.com>
2019-06-25 10:04:44 +0800

    349.intersection-of-two-arrays ok hash

--- 349.intersection-of-two-arrays.go
+++ 349.intersection-of-two-arrays.go
@@ -3,22 +3,23 @@
  *
  * [349] Intersection of Two Arrays
  */
-//  4 ms 94.88 %
-func intersection(nums1 []int, nums2 []int) []int {
+ func intersection(nums1 []int, nums2 []int) []int {
 	// hash 处理
 	// 注意去重
-	nummap := make(map[int]bool, 0)
+	nummap := make(map[int]int, 0)
 	for _,v := range nums1 {
 		// nummap[v]++ 通过赋值处理，而不是计数，计数可能吧一个数组中的数字返回
-		nummap[v] = true
+		nummap[v] = 1
 	}
 	for _,v := range nums2 {
-		nummap[v] = true && nummap[v]
+		if nummap[v] == 1 {
+			nummap[v] = 2
+		}
 	}
 
 	rtn := make([]int, 0, len(nums1))
 	for k,v := range nummap {
-		if v == true {
+		if v == 2 {
 			rtn = append(rtn, k)
 		}
 	}

ed1231c6ed43c16e41eaa87863848e219d178720
侯利朋 <houlipengdyx@163.com>
2019-06-27 15:51:57 +0800

    752.open-the-lock ok BFS 待优化

--- 752.open-the-lock.go
+++ 752.open-the-lock.go
@@ -3,23 +3,25 @@
  *
  * [752] Open the Lock
  */
+//  48 ms 73.68 %  string -> int 优化nexts函数
 //  132 ms 32.89 % 待优化
 //  图的BFS搜索最短路径
 // 4位数字，每个数字上下2个情况，一共对应8中情况
 func openLock(deadends []string, target string) int {
 	// map缓存，方便查找
-	deadendsMap := make(map[string]bool, len(deadends))
+	deadendsMap := make(map[int]bool, len(deadends))
 	for _,v := range deadends {
-		deadendsMap[v] = true
+		dig,_ := strconv.Atoi(v)
+		deadendsMap[dig] = true
 	}
-
-	return bfs("0000", target, deadendsMap) //通过deadendsMap当used使用？？？
+	targetDig,_ := strconv.Atoi(target)
+	return bfs(0, targetDig, deadendsMap) //通过deadendsMap当used使用？？？
 }
 
 // 广度优先， 非递归
-func bfs(start, target string, used map[string]bool) int{
+func bfs(start, target int, used map[int]bool) int{
 	step := -1 // 注意step初始值，否则可能多一个
-	queue := []string{start}
+	queue := []int{start}
 
 	for len(queue) > 0 {
 		l := len(queue)
@@ -45,19 +47,15 @@ func bfs(start, target string, used map[string]bool) int{
 }
 
 // 生成相邻的8个节点
-func nexts(start string) []string {
-	rtn := make([]string, 0, 8)
-	for i:=0; i<4; i++ {
-		if start[i] == '0' {
-			rtn = append(rtn,start[:i] + "1" + start[i+1:])
-			rtn = append(rtn,start[:i] + "9" + start[i+1:])
-		} else if start[i] == '9' {
-			rtn = append(rtn,start[:i] + "8" + start[i+1:])
-			rtn = append(rtn,start[:i] + "0" + start[i+1:])
-		} else {
-			rtn = append(rtn,start[:i] + string(start[i]+byte(1)) + start[i+1:])
-			rtn = append(rtn,start[:i] + string(start[i]-byte(1)) + start[i+1:])
-		}
+func nexts(start int) []int {
+	return []int{
+		start%1000 + (start/1000+1)%10*1000,
+		start%1000 + (start/1000-1+10)%10*1000,
+		start/1000*1000 + start%100 + (start%1000/100+1)%10*100,
+		start/1000*1000 + start%100 + (start%1000/100-1+10)%10*100,
+		start/100*100 + start%10 + (start%100/10+1)%10*10,
+		start/100*100 + start%10 + (start%100/10-1+10)%10*10,
+		start/10*10 + (start%10+1)%10,
+		start/10*10 + (start%10-1+10)%10,
 	}
-	return rtn
 }

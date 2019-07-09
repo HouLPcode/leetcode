19b2325993deeebaead7c359d0b30908221d2d9b
侯利朋 <houlipengdyx@163.com>
2019-07-09 10:43:56 +0800

    69.sqrt-x ok

--- 69.sqrt-x.go
+++ 69.sqrt-x.go
@@ -3,27 +3,20 @@
  *
  * [69] Sqrt(x)
  */
+//  0 ms
+// 折半查找
 func mySqrt(x int) int {
-	// 开方的解题思路
-	// 方法1： y = x^2 是单调函数，可以从 1->n 二分遍历
-	//TODO 方法2：牛顿迭代法 ？？？？？
-	
-	// 题目中指定x非负
-	if x == 0 || x == 1{
+	if x == 0 || x == 1 { // 题目中指定x非负
 		return x
 	}
-	left,right := 1,x
-	for left <= right{
-		mid := (left + right) / 2
-		if mid == x / mid { //注意：采用除法，防止 mid*mid 越界
-			return mid
-		} else if mid > x / mid{
-			right = mid -1
-		}else if mid < x / mid{
+	left, right := 1, x
+	for left <= right {
+		mid := (right-left)/2 + left
+		if mid > x/mid { //注意：采用除法，防止 mid*mid 越界
+			right = mid - 1
+		} else if mid <= x/mid {
 			left = mid + 1
 		}
 	}
-	// 跳出循环时，left^2 > x, right^2 < x,取较小值
-	return right
+	return left - 1
 }
-

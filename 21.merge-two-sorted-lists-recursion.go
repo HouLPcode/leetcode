26679dffa5cd234cf16e545dd6956855b16a30a1
侯利朋 <houlipengdyx@163.com>
2019-07-11 10:23:20 +0800

    .\21.merge-two-sorted-lists-recursion ok recursion

---
+++ 21.merge-two-sorted-lists-recursion.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=21 lang=golang
+ *
+ * [21] Merge Two Sorted Lists
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//   0 ms
+// recursion 递归实现
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}

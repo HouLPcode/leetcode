bf87b3cbc31e89676ed286d25299286941b4e5c5
侯利朋 <houlipengdyx@163.com>
2019-07-20 10:41:22 +0800

    231

--- 231.power-of-two.go
+++ 231.power-of-two.go
@@ -12,40 +12,35 @@
  * Testcase Example:  '1'
  *
  * Given an integer, write a function to determine if it is a power of two.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: 1
- * Output: true 
+ * Output: true
  * Explanation: 20 = 1
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: 16
  * Output: true
  * Explanation: 24 = 16
- * 
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: 218
  * Output: false
- * 
+ *
  */
 
- // 判断一个数是不是2的幂运算的值
+// 判断一个数是不是2的幂运算的值
+// 2的幂运算结果中二进制只有1个1bit
 func isPowerOfTwo(n int) bool {
-	if n <=0{
+	if n == 0 {
 		return false
 	}
-    for n & 1 ==0 {
-    	n >>= 1
-	}
-	if n == 1 {
-		return true
-	}
-	return false
+	return n&(n-1) == 0
 }

4842782bc6b3a9122638ca2548be444f42762b36
侯利朋 <houlipengdyx@163.com>
2019-07-29 16:55:30 +0800

    fix

--- 23.merge-k-sorted-lists.go
+++ 23.merge-k-sorted-lists.go
@@ -10,55 +10,55 @@
  *     Next *ListNode
  * }
  */
- // 方法1: 暴力合并，每轮筛选出一个最小node加入链表，O(kkn),k表示链表数，每个链表平均n个节点
+// 方法1: 暴力合并，每轮筛选出一个最小node加入链表，O(kkn),k表示链表数，每个链表平均n个节点
 //  方法2: map缓存后排序，O(knlog(kn))
- // 方法3: 分治法，两两合并 O(knlogk)  每个请款都差不多，两两合并，适合分治法
+// 方法3: 分治法，两两合并 O(knlogk)  每个情况都差不多，两两合并，适合分治法
 func mergeKLists(lists []*ListNode) *ListNode {
-    if len(lists) == 0{
+	if len(lists) == 0 {
 		return nil
-	}else if len(lists) == 1{
+	} else if len(lists) == 1 {
 		return lists[0]
-	}else if len(lists) == 2{
+	} else if len(lists) == 2 {
 		return mergeTwoLists(lists[0], lists[1])
-	}else{
+	} else {
 		l1 := mergeKLists(lists[:len(lists)/2])
 		l2 := mergeKLists(lists[len(lists)/2:])
 		return mergeTwoLists(l1, l2)
 	}
 	return nil // 不会到这一步
 }
+
 // copy leetcode 21
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-    if l1 == nil {
+	if l1 == nil {
 		return l2
-	}else if l2 == nil{
+	} else if l2 == nil {
 		return l1
 	}
 	var head, cur *ListNode
-	if l1.Val < l2.Val{
+	if l1.Val < l2.Val {
 		head, cur = l1, l1
 		l1 = l1.Next
-   	}else {
+	} else {
 		head, cur = l2, l2
 		l2 = l2.Next
-   	}
+	}
 
-	for l1!=nil && l2 != nil {
-		if l1.Val < l2.Val{
-			 cur.Next =  l1 
-			 cur = cur.Next
-			 l1 = l1.Next
-		}else {
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cur.Next = l1
+			cur = cur.Next
+			l1 = l1.Next
+		} else {
 			cur.Next = l2
-			cur= cur.Next
+			cur = cur.Next
 			l2 = l2.Next
 		}
 	}
-	if l1 != nil{
+	if l1 != nil {
 		cur.Next = l1
-	}else {
+	} else {
 		cur.Next = l2
 	}
 	return head
 }
-

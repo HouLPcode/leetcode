92cd32b01180603dc9705034714dec16e220d769
侯利朋 <houlipengdyx@163.com>
2019-07-31 12:22:12 +0800

    fix

--- 16.3-sum-closest.go
+++ 16.3-sum-closest.go
@@ -5,6 +5,7 @@ import "sort"
  *
  * [16] 3Sum Closest
  */
+//   8 ms, faster than 44.44%
 // [0,2,1,-3]\n1
 func threeSumClosest(nums []int, target int) int {
 	// 排序后遍历每个元素，再剩余元素中，首尾指针计算三个元素的和
@@ -18,8 +19,10 @@ func threeSumClosest(nums []int, target int) int {
 			}
 			if sum < target {
 				l++
-			} else {
+			} else if sum > target {
 				r--
+			} else {
+				return target // 等于的时候最小，直接返回
 			}
 		}
 	}

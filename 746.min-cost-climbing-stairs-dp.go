ce95c76c802239ea8606f2c2999607437fd7ff0d
侯利朋 <houlipengdyx@163.com>
2019-08-15 16:28:16 +0800

    .\746.min-cost-climbing-stairs ok dp

---
+++ 746.min-cost-climbing-stairs-dp.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=746 lang=golang
+ *
+ * [746] Min Cost Climbing Stairs
+ */
+// (4 ms) 92.57 %
+// 典型dp
+// dp[i] 表示跳到第i阶的最小花费 最终结果dp[0]
+// dp[i] = cost[i]+ min(dp[i+1]+, dp[i+2]) //从i往0/1 跳
+// 初始 dp[0] = 0, dp[1] = 0
+func minCostClimbingStairs(cost []int) int {
+	dp0, dp1 := 0, 0
+	rtn := 0
+	for i := len(cost) - 1; i >= 0; i-- {
+		rtn = cost[i] + min(dp0, dp1)
+		dp0, dp1 = dp1, rtn
+	}
+	return min(dp0, dp1)
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
--- 746.min-cost-climbing-stairs.go
+++ 746.min-cost-climbing-stairs.go
@@ -3,20 +3,20 @@
  *
  * [746] Min Cost Climbing Stairs
  */
-// (4 ms) 92.57 %
+// (4 ms) 92.67 %
 // 典型dp
-// dp[i] 表示跳到第i阶的最小花费 最终结果dp[0]
-// dp[i] = cost[i]+ min(dp[i+1]+, dp[i+2]) //从i往0/1 跳
+// dp[i] 表示跳到第i阶的最小花费 最终结果dp[l]
+// dp[i] = min(dp[i-1]+num[i-1], dp[i-2]+num[i-2])
 // 初始 dp[0] = 0, dp[1] = 0
 func minCostClimbingStairs(cost []int) int {
-	dp0, dp1 := 0, 0
-	rtn := 0
-	for i := len(cost) - 1; i >= 0; i-- {
-		rtn = cost[i] + min(dp0, dp1)
-		dp0, dp1 = dp1, rtn
+	n := len(cost) // n [2,1000]
+	pri1, pri2 := 0, 0
+	for i := 2; i <= n; i++ {
+		pri1, pri2 = pri2, min(pri1+cost[i-2], pri2+cost[i-1])
 	}
-	return min(dp0, dp1)
+	return pri2
 }
+
 func min(a, b int) int {
 	if a < b {
 		return a

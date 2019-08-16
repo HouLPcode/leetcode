8ba46ad5fea560fcaeb1ebc35ac2e0c40d1a48b2
侯利朋 <houlipengdyx@163.com>
2019-08-16 09:53:04 +0800

    fix

--- 338.counting-bits.go
+++ 338.counting-bits.go
@@ -14,44 +14,44 @@
  * Given a non negative integer number num. For every numbers i in the range 0
  * ≤ i ≤ num calculate the number of 1's in their binary representation and
  * return them as an array.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: 2
  * Output: [0,1,1]
- * 
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: 5
  * Output: [0,1,1,2,1,2]
- * 
- * 
+ *
+ *
  * Follow up:
- * 
- * 
+ *
+ *
 	* It is very easy to come up with a solution with run time
 	* O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a
 	* single pass?
  * Space complexity should be O(n).
  * Can you do it like a boss? Do it without using any builtin function like
  * __builtin_popcount in c++ or in any other language.
- * 
- */
-
- // 循环遍历，统计每个数中1的个数，统计方式采用191题的汉明权重
+ *
+*/
+// 另一个方法，类似dp求解
+// 循环遍历，统计每个数中1的个数，统计方式采用191题的汉明权重
 func countBits(num int) []int {
 	rnt := []int{}
-	for i:=int(0);i<=num; i++{		
-		rnt = append(rnt,hmweight(i))
+	for i := int(0); i <= num; i++ {
+		rnt = append(rnt, hmweight(i))
 	}
 	return rnt
 }
 
-func hmweight(num int)int{
-	var cnt int 
-	for ;num != 0; num &=(num-1){
+func hmweight(num int) int {
+	var cnt int
+	for ; num != 0; num &= (num - 1) {
 		cnt++
 	}
 	return cnt

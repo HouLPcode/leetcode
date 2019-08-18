a980a9b401dd25a94ae274b62078aeaac9f9a3bf
侯利朋 <houlipengdyx@163.com>
2019-08-18 16:37:22 +0800

    fix 3

--- 3.longest-substring-without-repeating-characters.go
+++ 3.longest-substring-without-repeating-characters.go
@@ -3,23 +3,36 @@
  *
  * [3] Longest Substring Without Repeating Characters
  */
- // 两个指针组成子序列，map中缓存字母出现的位置，不是出现过没
+// (0 ms) √ Your runtime beats 100 %
+// 两个指针组成子序列，map中缓存字母出现的位置，不是出现过没
 // ✘ testcase: '"abba"'
+// " " // 注意，不是只有小写字母的
 func lengthOfLongestSubstring(s string) int {
-	count := 0
-	// alpMap := make(map[byte]bool, 0)
-	alpMap := make(map[byte]int, 128)// byte最多128个，string长度不超过intMAX
-	for p1,p2:=0,0; p2<len(s); p2++{//只循环右指针，左指针通过map缓存进行赋值
-		if val,ok := alpMap[s[p2]]; ok{ // 字符出现过
-			//如果此处应该把p1到val+1出现的字母从map中删除，则下面这句判断可以不写
-			if val+1 > p1 { //***一定注意此处判断：重复字母的出现位置可能在p1之前，此时p1p2构成的子串中没有重复元素，p1不用移动
-				p1 = val+1//左指针改为重复字符后一个字符，p1到val+1之间的字母虽然在map中仍然存在，但是在子串中是不存在的，无效的。p1只增不减	
-		   }
-		}
-		alpMap[s[p2]] = p2//添加或更新字母出现的位置
-		if p2-p1+1 > count{
-			count = p2-p1+1
+	buf := make([]int, 128) // 存储每个字符出现的位置
+	for i := 0; i < 128; i++ {
+		buf[i] = -1
+	}
+	maxLen := 0
+	left, right := 0, 0
+	for ; right < len(s); right++ {
+		if buf[int(s[right])] == -1 {
+			buf[int(s[right])] = right // 没有出现过
+		} else { // 出现过
+			if right-left > maxLen {
+				maxLen = right - left
+			}
+			// 注意：更新l坐标的时候，需要把l移动过程中出现的元素全部还原
+			i := left
+			for ; i < buf[int(s[right])]+1; i++ {
+				buf[int(s[i])] = -1
+			}
+			left = i
+			buf[int(s[right])] = right // 注意，不要忘了在此处更新该字符出现的位置
 		}
 	}
-	return count
+	if right-left > maxLen {
+		maxLen = right - left
+	}
+
+	return maxLen
 }

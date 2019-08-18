c38106d4e20c91b54a43b23e992fa1e6c32dba29
侯利朋 <houlipengdyx@163.com>
2019-08-18 13:19:16 +0800

    fix

--- 239.sliding-window-maximum.go
+++ 239.sliding-window-maximum.go
@@ -8,15 +8,17 @@
 // [1,-1]\n1
 // 最大值队列
 //  入队：空，直接放入。值>=队尾，弹出队尾，继续比较。值<队尾，放入
+// 注意：queue中存放的是index， 不是具体的数据值
 func maxSlidingWindow(nums []int, k int) []int {
 	rtn := make([]int, 0, len(nums))
 	if len(nums) == 0 || k == 0 {
 		return rtn
 	}
+
 	maxQueue := make([]int, 0, k)
+
 	maxQueue = append(maxQueue, 0)
-	curMax := nums[0]
-	rtn = append(rtn, curMax)
+	rtn = append(rtn, nums[0])
 	for i := 1; i < len(nums); i++ {
 		if len(maxQueue) > 0 && i == maxQueue[0]+k {
 			// 出队
@@ -27,8 +29,7 @@ func maxSlidingWindow(nums []int, k int) []int {
 			maxQueue = maxQueue[:len(maxQueue)-1]
 		}
 		maxQueue = append(maxQueue, i) // 注意队列里面放的是下标，不是内容。方便出栈时的比较
-		curMax = nums[maxQueue[0]]
-		rtn = append(rtn, curMax)
+		rtn = append(rtn, nums[maxQueue[0]])
 	}
 	return rtn[k-1:] // 舍头
 }
